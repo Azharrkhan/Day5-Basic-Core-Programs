0b4d286370bd9e6713f13838690c091515bb056a
Azhar <azharumi786@gmail.com>
2022-09-01 21:21:33 +0530

    Added OddEven

---
+++ OddEven.java
@@ -0,0 +1,24 @@
+package com.bl.basiccoreprograms;
+
+import java.util.Scanner;
+
+public class OddEven {
+	
+		static void EvenOdd(){
+
+		System.out.println("Enter a number");
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if ( num % 2 == 0){
+			System.out.println("The given number is even");
+		}
+		else {
+			System.out.println("The given number is not even");
+		}
+	}
+		public static void main(String[] args) {
+			EvenOdd();
+	}
+}
+
