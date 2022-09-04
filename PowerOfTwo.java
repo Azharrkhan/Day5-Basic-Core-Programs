089e2029ad77d8542dcc6c8e04b5ef9ec516cb65
Azhar <azharumi786@gmail.com>
2022-09-04 17:00:25 +0530

    added poweroftwo

---
+++ PowerOfTwo.java
@@ -0,0 +1,14 @@
+package com.bl.basiccoreprograms;
+
+public class PowerOfTwo {
+	public static void main(String[] args) {
+		double base = 2;
+		double N = 30;
+		for(N = 0; N <= 30; N++) {
+		double result = Math.pow( base, N);
+		{
+			System.out.println(result);
+		}
+		}
+	}
+}
